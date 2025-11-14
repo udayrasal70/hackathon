421029ec1b1bb5802547f8570bedbfc397c02dac
udayrasal70 <rasaluday70@gmail.com>
2025-11-14 13:27:37 +0530

    my task

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,30 +2,18 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Register from './pages/Register/Register'
-<<<<<<< HEAD
-import Login from './pages/Login/Login'
-import Sharereview from './pages/Sharereview/Sharereview'
-import Editreview from './pages/Editreview/Editreview'
-import Deletereview from './pages/Deletereview/Deletereview'
-import Createreview from './pages/Createreview/Createreview'
 
-=======
 import AllReviews from './pages/AllReviews/AllReviews'
 import MyReviews from "./pages/MyReviews/MyReviews"
 import AllMovies from './pages/AllMovies/AllMovies'
 import EditProfile from "./pages/EditProfile/EditProfile"
 import ChangePassword from './pages/ChangePassword/ChangePassword'
 import NavBar from './components/NavBar/NavBar'
->>>>>>> 2c0492a211875adcc378ab5a1aa8986feda6c870
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-<<<<<<< HEAD
-      <Createreview></Createreview>
-=======
       {/* <Register></Register> */}
       <NavBar></NavBar>
       <AllReviews></AllReviews>
@@ -35,7 +23,6 @@ function App() {
       {/* <ChangePassword></ChangePassword> */}
 
 
->>>>>>> 2c0492a211875adcc378ab5a1aa8986feda6c870
     </>
   )
 }
